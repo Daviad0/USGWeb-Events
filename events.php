5698f1b8db5391fc3512a90a679083b794f9757e
David Reeves <djreeves2005@gmail.com>
2024-11-07 14:20:12 -0500

    setting up for php

---
+++ events.php
@@ -0,0 +1,15 @@
+<!-- PHP to retrieve events from database -->
+
+<?php
+    $title = "Events";
+?>
+
+<html>
+    <head>
+        <title>Events</title>
+    </head>
+    <body>
+        <img src="resources/Crest_edited.png"/>
+        <h1><?php $title ?></h1>
+    </body>
+</html>
