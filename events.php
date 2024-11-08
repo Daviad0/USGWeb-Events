412fb0b151f82d3ac96b6e14c7a612431051ecf2
David Reeves <djreeves2005@gmail.com>
2024-11-08 11:12:43 -0500

    changes to the general design

--- events.php
+++ events.php
@@ -9,7 +9,16 @@
         <title>Events</title>
     </head>
     <body>
+        <div class="heading">
+            <img src="resources/Crest_edited.png"/>
+            <div>
+            <span class="huge">Undergraduate Student Government</span>
+            <span class="small">Meetings & Events</span>
+            </div>
+            
+        </div>
+
         <img src="resources/Crest_edited.png"/>
-        <h1><?php $title ?></h1>
+        <h1><?php echo $title ?></h1>
     </body>
 </html>
